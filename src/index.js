038f619da77854c173bb61423043d7e0c075c3b6
benclmnt <bennettclement@gmail.com>
2019-12-28 10:56:15 +0800

    Refactor itin reducers to follow api response

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './store/reducers/rootReducer'
 import { Provider } from 'react-redux'
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
+);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
--- src/pages/Itin.js
+++ src/pages/Itin.js
@@ -1,48 +1,57 @@
-import React, { Component } from 'react'
+import React, {Fragment} from 'react'
 import { connect } from 'react-redux'
 import { removePlan } from '../store/actions/planActions'
 
-class Itin extends Component {
+const Itin = (props) => {
+	const handleRemove = (id, slug) => {
+		console.log(id, slug)
+		props.removePlan(id, slug);
+	}
 
-    handleRemove = (id) => {
-        console.log(id)
-        this.props.removePlan(id);
-    }
+	console.log(props.itin)
+	let planList = Object.keys(props.itin).length > 0 ? (
+		Object.entries(props.itin).map(([slug, plans]) => {
+			return(
+				<Fragment key={slug}>
+					<h6 className="red-text">{ slug }</h6>
+					<ul>
+					{
+						plans.length > 0 
+							? plans.map(plan => (
+								<li key={ plan._id }>
+									{ plan.name }
+									<br/>
+									<button onClick = { () => handleRemove(plan._id, slug) }> Remove this plan </button>
+								</li>
+							))
+							: <p>You have deleted all plans for this area</p>
+					}
+					</ul>
+				</Fragment>
+			)
+		})
+	) : (
+		<p> You haven't make any plans yet </p>
+	) 
 
-    render() {
-        let planList = this.props.itin.length ? (
-            this.props.itin.map(plan => {
-                return(
-                    <li key={ plan.id }>
-                        { plan.title }
-                        <br/>
-                        <button onClick = { () => this.handleRemove(plan.id) }> Remove this plan </button>
-                    </li>
-                )
-            })
-        ) : (
-            <p> You haven't make any plans yet </p>
-        ) 
-        return (
-            <div className='container'>
-                <ul>
-                    { planList }
-                </ul>
-            </div>
-        )
-    }
-}
+	return (
+		<div className='container'>
+			{ planList }
+		</div>
+	)
+	{/* return(<div>Hello World</div>) */}
+	}
 
 const mapStateToProps = (state) => {
     console.log(state)
     return {
-        itin: state.plan.plan
+        itin: state.plan.itin
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return{
-        removePlan : id => { dispatch(removePlan(id)) }
+        removePlan : (id, slug) => { dispatch(removePlan(id, slug)) }
     }
 }
 
