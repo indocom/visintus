3193606940d4e3ca1aa8ab49e264f0bb22bdd15d
benClmnt <49342399+benClmnt@users.noreply.github.com>
2019-12-28 12:30:38 +0800

    Temporarily Fix CORS (#2)
    * Update seed data and config for production
    
    * Fix bugs in package scripts
    
    * Fix CORS problem
    
    Co-authored-by: Steven Wijaya <stevenwjy149@gmail.com>

--- config/router.js
+++ config/router.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+router.options("/*", function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Access-Control-Request-Method, Access-Control-Request-Headers');
+  res.header('Access-Control-Allow-Credentials', true)
+  res.send(200);
+});
+
 const categoriesController = require('../app/controllers/categories.js');
 
 router.get('/', (req, res) => {
