c3dd5a075a6905f700c05a33de911a728276f84b
RevolutionDz <audreyanwar@gmail.com>
2020-01-01 21:49:08 +0700

    added initial design for dropdown

--- src/pages/Itin.js
+++ src/pages/Itin.js
@@ -1,6 +1,7 @@
-import React, {Fragment} from 'react'
-import { connect } from 'react-redux'
-import { removePlan } from '../store/actions/planActions'
+import React, {Fragment} from 'react';
+import { connect } from 'react-redux';
+import { removePlan } from '../store/actions/planActions';
+import '../css/itin.css';
 
 const Itin = (props) => {
 	const handleRemove = (id, slug) => {
@@ -22,20 +23,24 @@ const Itin = (props) => {
 		Object.entries(props.itin).map(([slug, plans]) => {
 			return(
 				<Fragment key={slug}>
-					<h6 className="red-text">{ slug }</h6>
-					<ul>
-					{
-						plans.length > 0 
-							? plans.map(plan => (
-								<li key={ plan._id }>
-									{ plan.name }
-									<br/>
-									<button onClick = { () => handleRemove(plan._id, slug) }> Remove this plan </button>
-								</li>
-							))
-							: <p>You have deleted all plans for this area</p>
-					}
-					</ul>
+					<div className='dropdownItin__title'>
+						<h6 className="red-text">{ slug }</h6>
+						<button>Click Me!</button>
+					</div>
+					<div className='dropdownItin__content'>
+						<ul>
+						{
+							plans.length > 0 
+								? plans.map(plan => (
+									<li key={ plan._id }>
+										<p>{ plan.name }</p>
+										<button onClick = { () => handleRemove(plan._id, slug) }> Remove this plan </button>
+									</li>
+								))
+								: <li>You have deleted all plans for this area</li>
+						}
+						</ul>
+					</div>
 				</Fragment>
 			)
 		})
@@ -44,7 +49,7 @@ const Itin = (props) => {
 	) 
 
 	return (
-		<div className='container'>
+		<div className='dropdownItin'>
 			{ planList }
 			<div className="btn" onClick={handleCheckout}>Checkout</div>
 		</div>
