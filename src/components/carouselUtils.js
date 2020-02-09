be08030dda06e73b8ac49755c6847912d95e1ac3
Audrey Felicio Anwar <54733401+AudreyFelicio@users.noreply.github.com>
2020-02-09 22:42:29 +0800

    Add highlight description and link (#17)
    * Added initial highlight description
    
    * Added highlight caption functionality
    
    * added hyperlink and fixed caption
    
    * Clean up code + update highlights hyperlink
    
    Co-authored-by: benClmnt <49342399+benClmnt@users.noreply.github.com>

--- src/components/carouselUtils.js
+++ src/components/carouselUtils.js
@@ -9,6 +9,10 @@ export default function carouselUtils() {
   const lastSlide = slides[slides.length - 1];
   const beginningDot = dots[0];
   const lastDot = dots[dots.length - 1];
+  const captions = document.querySelector('.carousel__captions');
+  const caption = Array.from(captions.children);
+  const beginningCaption = caption[0];
+  const lastCaption = caption[caption.length - 1];
 
   const slideSize = slides[0].getBoundingClientRect();
   const slideWidth = slideSize.width;
@@ -17,6 +21,11 @@ export default function carouselUtils() {
     slide.style.left = slideWidth * index + 'px';
   };
   slides.forEach(setSlidePosition);
+
+  const moveToCaption = (currentCaption, targetCaption) => {
+    currentCaption.classList.remove('current-slide');
+    targetCaption.classList.add('current-slide');
+  };
 
   const moveToSlide = (track, currentSlide, targetSlide) => {
     track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
@@ -48,17 +57,23 @@ export default function carouselUtils() {
     const currentDot = dotsNav.querySelector('.current-slide');
     const nextDot = currentDot.nextElementSibling;
     const currentIndex = slides.findIndex(slide => slide === currentSlide);
+    const currentCaption = captions.querySelector('.current-slide');
+    const nextCaption = currentCaption.nextElementSibling;
 
     if (currentIndex !== slides.length - 1) {
       //move to next slide
       moveToSlide(track, currentSlide, nextSlide);
       //move dot
       updateDots(currentDot, nextDot);
+      //move caption
+      moveToCaption(currentCaption, nextCaption);
     } else {
       //move to beginning slide
       moveToSlide(track, currentSlide, beginningSlide);
       //move dot
       updateDots(currentDot, beginningDot);
+      //move caption
+      moveToCaption(currentCaption, beginningCaption);
     }
     //restart automatic movement
     clearInterval(timer);
@@ -72,17 +87,23 @@ export default function carouselUtils() {
     const currentDot = dotsNav.querySelector('.current-slide');
     const prevDot = currentDot.previousElementSibling;
     const currentIndex = slides.findIndex(slide => slide === currentSlide);
+    const currentCaption = captions.querySelector('.current-slide');
+    const prevCaption = currentCaption.previousElementSibling;
 
     if (currentIndex !== 0) {
       //move to previous slide
       moveToSlide(track, currentSlide, prevSlide);
       //move dot
       updateDots(currentDot, prevDot);
+      //move caption
+      moveToCaption(currentCaption, prevCaption);
     } else {
       //move to last slide
       moveToSlide(track, currentSlide, lastSlide);
       //move dot
       updateDots(currentDot, lastDot);
+      //move caption
+      moveToCaption(currentCaption, lastCaption);
     }
     clearInterval(timer);
     setTimer(3000);
@@ -97,12 +118,16 @@ export default function carouselUtils() {
     const currentDot = dotsNav.querySelector('.current-slide');
     const targetIndex = dots.findIndex(dot => dot === targetDot);
     const targetSlide = slides[targetIndex];
+    const currentCaption = captions.querySelector('.current-slide');
+    const targetCaption = caption[targetIndex];
 
     if (targetDot !== null) {
       //move to target slide
       moveToSlide(track, currentSlide, targetSlide);
       //move dot
       updateDots(currentDot, targetDot);
+      //move caption
+      moveToCaption(currentCaption, targetCaption);
     }
     clearInterval(timer);
     setTimer(3000);
@@ -115,16 +140,22 @@ export default function carouselUtils() {
     const currentDot = dotsNav.querySelector('.current-slide');
     const nextDot = currentDot.nextElementSibling;
     const currentIndex = slides.findIndex(slide => slide === currentSlide);
+    const currentCaption = captions.querySelector('.current-slide');
+    const nextCaption = currentCaption.nextElementSibling;
     if (currentIndex !== slides.length - 1) {
       //move to next slide
       moveToSlide(track, currentSlide, nextSlide);
       //move dot
       updateDots(currentDot, nextDot);
+      //move caption
+      moveToCaption(currentCaption, nextCaption);
     } else {
       //move to beginning slide
       moveToSlide(track, currentSlide, beginningSlide);
       //move dot
       updateDots(currentDot, beginningDot);
+      //move caption
+      moveToCaption(currentCaption, beginningCaption);
     }
   }
   let timer = null;
