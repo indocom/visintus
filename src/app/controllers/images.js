19f0208d8bfd960a7a60276353b958b7dcb922b6
Bennett Clement <bennettclement@gmail.com>
2020-02-25 19:07:44 +0800

    Handle single file upload

--- src/app/controllers/images.js
+++ src/app/controllers/images.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const uuid = require('uuid');
 const config = require('../../config');
+const multer = require('multer');
+const path = require('path');
 
 const {
   handleError,
@@ -9,7 +11,24 @@ const {
   buildSuccObject
 } = require('../middleware/utils.js');
 
+var storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, path.join(__dirname, '/../../../public/images'));
+  },
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+var upload = multer({ storage: storage }).single('file');
+
 exports.upload = async (req, res) => {
-  console.log(req);
-  handleSuccess(res, buildSuccObject('https://source.unsplash.com/random'));
+  upload(req, res, function(err) {
+    if (err instanceof multer.MulterError) {
+      handleError(res, buildErrObject(500, err));
+    } else if (err) {
+      handleError(res, buildErrObject(500, err));
+    }
+    handleSuccess(res, buildSuccObject(req.file));
+  });
 };
