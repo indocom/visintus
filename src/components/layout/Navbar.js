2178c07605d82356a3b5bf9a1e31fa6c5c07cb8f
Bennett Clement <bennettclement@gmail.com>
2020-03-18 00:32:26 +0800

    Responsive navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { logOutUser } from '../../store/actions/authActions.js';
 import M from 'materialize-css';
+import Logo from '../../assets/logo-whitetxt.png';
 
 const Navbar = props => {
   const isLoggedIn = localStorage.getItem('isLoggedIn');
@@ -14,6 +15,12 @@ const Navbar = props => {
   useEffect(() => {
     let elems = document.querySelectorAll('.dropdown-trigger');
     M.Dropdown.init(elems);
+  }, []);
+
+  useEffect(() => {
+    let elems = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems);
+    console.log('hello');
   }, []);
 
   const handleLogout = e => {
@@ -35,55 +42,112 @@ const Navbar = props => {
   };
 
   return (
-    <nav className="nav-wrapper black">
-      <ul id="dropdownLogout" className="dropdown-content">
+    <>
+      <ul id="nav-mobile" className="sidenav">
         <li>
-          <Link to="/">Account</Link>
+          <a id="logo-container" href="#landing">
+            <img
+              src={Logo}
+              alt="PINUS Logo"
+              style={{ height: 50, marginTop: 5 }}
+            />
+          </a>
         </li>
-        {['admin', 'superadmin'].includes(role) && (
-          <li>
-            <Link to="/admin">Admin</Link>
-          </li>
-        )}
         <li>
-          <Link to="/" onClick={handleLogout}>
-            Logout
-          </Link>
+          <a target="_blank" href="https:pi-nus.org">
+            PINUS Homepage
+          </a>
         </li>
-      </ul>
-      <div className="container">
-        <Link to="/" className="brand-logo">
-          Visintus
-        </Link>
-        <ul className="right">
-          <li>
-            <a target="_blank" href="https:pi-nus.org">
-              PINUS Homepage
-            </a>
-          </li>
-          <li>
-            <NavLink to="/itinerary">Itinerary</NavLink>
-          </li>
-          {isLoggedIn === 'true' ? (
+        <li>
+          <NavLink to="/itinerary">Itinerary</NavLink>
+        </li>
+        {isLoggedIn === 'true' ? (
+          <>
             <li>
-              <a
-                className="dropdown-trigger"
-                href="#!"
-                data-target="dropdownLogout"
-              >
-                {initials}{' '}
-                <i
-                  className="fas fa-chevron-down"
-                  style={{ fontSize: '1rem' }}
-                ></i>
+              <NavLink to="/">Account</NavLink>
+            </li>
+            {['admin', 'superadmin'].includes(role) && (
+              <li>
+                <Link to="/admin">Admin</Link>
+              </li>
+            )}
+            <li>
+              <Link to="/" onClick={handleLogout}>
+                Logout
+              </Link>
+            </li>
+          </>
+        ) : (
+          <li>{/* <NavLink to="/login">Login</NavLink> */}</li>
+        )}
+      </ul>
+
+      <nav className="nav-wrapper black ">
+        <a href="#" data-target="nav-mobile" className="sidenav-trigger">
+          <i className="fas fa-bars"></i>
+        </a>
+        <img
+          src={Logo}
+          class="hide-on-large-only"
+          alt="PINUS Logo"
+          style={{
+            height: 40,
+            marginTop: 8,
+            position: 'absolute',
+            left: 'calc(50% - 70px)'
+          }}
+        />
+
+        <ul id="dropdownLogout" className="dropdown-content">
+          <li>
+            <Link to="/">Account</Link>
+          </li>
+          {['admin', 'superadmin'].includes(role) && (
+            <li>
+              <Link to="/admin">Admin</Link>
+            </li>
+          )}
+          <li>
+            <Link to="/" onClick={handleLogout}>
+              Logout
+            </Link>
+          </li>
+        </ul>
+
+        <div className="container hide-on-med-and-down">
+          <Link to="/" className="brand-logo">
+            Visintus
+          </Link>
+          <ul className="right">
+            <li>
+              <a target="_blank" href="https:pi-nus.org">
+                PINUS Homepage
               </a>
             </li>
-          ) : (
-            <li>{/* <NavLink to="/login">Login</NavLink> */}</li>
-          )}
-        </ul>
-      </div>
-    </nav>
+            <li>
+              <NavLink to="/itinerary">Itinerary</NavLink>
+            </li>
+            {isLoggedIn === 'true' ? (
+              <li>
+                <a
+                  className="dropdown-trigger"
+                  href="#!"
+                  data-target="dropdownLogout"
+                >
+                  {initials}{' '}
+                  <i
+                    className="fas fa-chevron-down"
+                    style={{ fontSize: '1rem' }}
+                  ></i>
+                </a>
+              </li>
+            ) : (
+              <li>{/* <NavLink to="/login">Login</NavLink> */}</li>
+            )}
+          </ul>
+        </div>
+      </nav>
+    </>
   );
 };
 
