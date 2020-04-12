55d189984cf232bb8d9b850d43e2f540a5c91ba0
Audrey Felicio Anwar <54733401+AudreyFelicio@users.noreply.github.com>
2020-04-12 12:57:29 +0800

    Add new footer (#3)
    * add new footer
    
    * Update minor styling
    
    Co-authored-by: Steven Wijaya <stevenwjy149@gmail.com>

--- webclient/src/components/layout/Footer.js
+++ webclient/src/components/layout/Footer.js
@@ -1,75 +1,88 @@
 import React from 'react';
+import '../../css/footer.css';
+import Logo from '../../assets/images/logo.png';
 
 const Footer = props => {
   return (
     <>
-      <footer className="page-footer black darken-4">
-        <div className="container">
-          <div className="row">
-            <div className="col s12 m4">
-              <h6>Perhimpunan Indonesia NUS</h6>
-              <ul>
-                <li>National University of Singapore</li>
-                <li>Yusof Ishak House, Level 3</li>
-                <li>31 Lower Kent Ridge Road</li>
-                <li>Singapore 119078</li>
-              </ul>
-            </div>
-
-            <div className="col s12 m6">
-              <div className="row">
-                <div className="col s12 m5">
-                  <h6>Contact Us</h6>
-                  <ul>
-                    <li>
-                      <i className="fas fa-phone"></i> 1234567
-                    </li>
-                    <li>
-                      <i className="fas fa-envelope"></i> pinus@pohon.id
-                    </li>
-                  </ul>
-                </div>
-                <div className="col s12 m7" id="social">
-                  <h6 className="white-text">Follow us</h6>
-                  <a
-                    href="https://www.facebook.com/PerhimpunanIndonesiaNUS/"
-                    className="btn-floating btn indigo darken-3"
-                  >
-                    <i className="fab fa-facebook-f"></i>
-                  </a>
-                  <a
-                    href="https://www.instagram.com/pinusonline/"
-                    className="btn-floating btn indigo darken-3"
-                  >
-                    <i className="fab fa-instagram"></i>
-                  </a>
-                  <a
-                    href="https://www.youtube.com/channel/UCfYU_ttUpJWNEKIbiWoQQHQ/"
-                    className="btn-floating btn indigo darken-3"
-                  >
-                    <i className="fab fa-youtube"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            <div className="col s2">
-              <h6>
-                <a href="#" className="white-text">
-                  <i
-                    className="fas fa-arrow-up"
-                    style={{ paddingRight: '10px' }}
-                  ></i>{' '}
-                  Top
-                </a>
-              </h6>
-            </div>
-          </div>
+      <footer class="footer">
+        <div className="footer__logo">
+          <img
+            src={Logo}
+            alt="Logo"
+            style={{ height: 100, marginTop: 10, marginBottom: -10 }}
+          />
+          <h3>VISIT</h3>
+          <h5>Perhimpunan Indonesia NUS</h5>
         </div>
-        <div className="footer-copyright">
-          <div className="container center">
-            Copyright &copy; PINUS 2019. All rights reserved.
-          </div>
+        <div className="footer__links">
+          <ul className="footer__col">
+            <li style={{ borderColor: '#ff0000' }}>
+              <h5>USEFUL LINKS</h5>
+            </li>
+            <li>
+              <a href="https://pi-nus.org" style={{ color: 'inherit' }}>
+                PINUS Homepage
+              </a>
+            </li>
+            <li>
+              <a href="https://visit.pi-nus.org">Visit Homepage</a>
+            </li>
+            <li>
+              <a href="https://visit.pi-nus.org/itinerary">Itinerary</a>
+            </li>
+          </ul>
+          <ul className="footer__col">
+            <li style={{ borderColor: '#efb61e' }}>
+              <h5>CONTACT US</h5>
+            </li>
+            <li className="footer__col__address">
+              National University of Singapore <br />
+              Yusof Ishak House, Level 3 <br />
+              31 Lower Kent Ridge Road <br />
+              Singapore 119078
+            </li>
+            <li>
+              <div>
+                <i className="fas fa-phone"> </i> &nbsp; +65 9160 1877
+              </div>
+            </li>
+            <li>
+              <div>
+                <i className="fas fa-envelope"></i> &nbsp; visit@pi-nus.org
+              </div>
+            </li>
+          </ul>
+          <ul className="footer__col">
+            <li style={{ borderColor: '#1f2e6e' }}>
+              <h5>FOLLOW US</h5>
+            </li>
+            <li style={{ border: 'none' }}>
+              <div className="col s10 offset-s1 l4">
+                <a
+                  href="https://www.facebook.com/PerhimpunanIndonesiaNUS/"
+                  className="btn-floating btn black"
+                >
+                  <i className="fab fa-facebook-f"></i>
+                </a>
+                <a
+                  href="https://www.instagram.com/pinusonline/"
+                  className="btn-floating btn black"
+                >
+                  <i className="fab fa-instagram"></i>
+                </a>
+                <a
+                  href="https://www.youtube.com/channel/UCfYU_ttUpJWNEKIbiWoQQHQ/"
+                  className="btn-floating btn black"
+                >
+                  <i className="fab fa-youtube"></i>
+                </a>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="ending">
+          Copyright @2020 PINUS. All rights reserved.
         </div>
       </footer>
     </>
