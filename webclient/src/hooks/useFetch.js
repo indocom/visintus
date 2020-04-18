e37c38764c4211072830f4e33b541c62cff66b4e
Steven Wijaya <stevenwjy149@gmail.com>
2020-04-18 18:11:35 +0800

    Setup new routing for backend

--- webclient/src/hooks/useFetch.js
+++ webclient/src/hooks/useFetch.js
@@ -14,7 +14,6 @@ const useFetch = ({ endpoint, needAuthorization = true }) => {
       setLoading(true);
       try {
         let headers = {
-          'Access-Control-Allow-Origin': '*',
           'Content-Type': 'application/json'
         };
 
