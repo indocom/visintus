d6566221fbd409e1f517899f8fff84b59a4f4846
Bennett Clement <bennettclement@gmail.com>
2020-04-19 19:59:12 +0800

    Refactor home component

--- webclient/src/components/Loading.js
+++ webclient/src/components/Loading.js
@@ -2,19 +2,20 @@ import React from 'react';
 
 function Loading() {
   return (
-    <div className="preloader-wrapper big active">
-      <div className="spinner-layer spinner-blue">
-        <div className="circle-clipper left">
-          <div className="circle"></div>
-        </div>
-        <div className="gap-patch">
-          <div className="circle"></div>
-        </div>
-        <div className="circle-clipper right">
-          <div className="circle"></div>
-        </div>
-      </div>
-    </div>
+    // <div className="preloader-wrapper big active">
+    //   <div className="spinner-layer spinner-blue">
+    //     <div className="circle-clipper left">
+    //       <div className="circle"></div>
+    //     </div>
+    //     <div className="gap-patch">
+    //       <div className="circle"></div>
+    //     </div>
+    //     <div className="circle-clipper right">
+    //       <div className="circle"></div>
+    //     </div>
+    //   </div>
+    // </div>
+    <div style={{ minHeight: '50vh' }}></div>
   );
 }
 
--- webclient/src/store/actions/authActions.js
+++ webclient/src/store/actions/authActions.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { getParsedCommandLineOfConfigFile } from 'typescript';
 
 const loginSuccess = token => {
   return {
