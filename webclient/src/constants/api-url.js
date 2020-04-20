67d03a76e9eb8094866bc7448a97f32335cdc4aa
Bennett Clement <bennettclement@gmail.com>
2020-04-20 09:55:24 +0800

    Refactor user admin component

--- webclient/src/constants/api-url.js
+++ webclient/src/constants/api-url.js
@@ -5,3 +5,6 @@ export const API_CHECKOUT = 'api/checkout';
 export const API_FORGOT_PSWD = 'api/users/forgot-password';
 export const API_RESET_PSWD = 'api/users/reset-password';
 export const API_VERIFY = 'api/users/verify';
+
+export const API_ADMIN_USERS = 'api/admin/users/list';
+export const API_ADMIN_USERS_UPDATE = 'api/admin/users/update-role';
--- webclient/src/constants/query-keys.js
+++ webclient/src/constants/query-keys.js
@@ -2,3 +2,5 @@ export const QUERY_KEY_CATEGORY = 'category';
 export const QUERY_KEY_HIGHLIGHTS = 'highlights';
 export const QUERY_KEY_CATEGORIES = 'categories';
 export const QUERY_KEY_PLAN_INFO = 'plan-info';
+
+export const QUERY_KEY_ADMIN_USERS = 'admin-users';
