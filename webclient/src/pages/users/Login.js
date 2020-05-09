7bffc4cf0c131e72df33359cf1e5a2a27b1dbabe
Bennett Clement <bennettclement@gmail.com>
2020-05-09 13:03:31 +0800

    Refactor auth-related components to use context

--- webclient/src/pages/users/Login.js
+++ webclient/src/pages/users/Login.js
@@ -1,100 +1,52 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { signInUser, logOutUser } from '../../store/actions/authActions';
+import { useAuth } from '~/context/auth-context';
+import { isLoggedIn } from '~/utils/auth-client';
 
-class Login extends Component {
-  state = {
+function Login(props) {
+  const [state, setState] = useState({
     email: '',
     password: ''
-  };
+  });
 
-  componentDidMount() {
-    const token = localStorage.getItem('token');
-  }
+  const authClient = useAuth();
 
-  handleReload = e => {
-    window.location.replace('/');
-  };
-
-  handleChange = e => {
-    this.setState({
+  const handleChange = e => {
+    setState({
+      ...state,
       [e.target.id]: e.target.value
     });
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    if (this.props.isLoggedIn) {
-      const token = localStorage.getItem('token');
-      this.props.logOutUser(token);
-      localStorage.setItem('token', null);
-      localStorage.setItem('isLoggedIn', false);
-      localStorage.setItem('role', null);
-      localStorage.setItem('initials', null);
-    } else {
-      const awaitSignIn = new Promise((resolve, reject) => {
-        this.props.signInUser(this.state);
-        setTimeout(() => resolve(this.props.authError), 500);
-      });
-      awaitSignIn.then(value => {
-        if (!value) {
-          this.handleReload();
-        }
-      });
-      //setTimeout(() => this.handleReload(), 1000);
-    }
+    authClient.login(state);
   };
-  render() {
-    const { authError, isLoggedIn, auth } = this.props;
-    //if (auth.uid) return <Redirect to='/' />
 
-    return (
-      <div className="container">
-        <form className="grey lighten-4" onSubmit={this.handleSubmit}>
-          <h5 className="grey-text text-darken-3">Sign In</h5>
-          <div className="input-field">
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" onChange={this.handleChange} />
-          </div>
-          <div className="input-field">
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" onChange={this.handleChange} />
-          </div>
-          <div className="input-field">
-            {isLoggedIn ? (
-              <p>You are logged in</p>
-            ) : (
-              <button className="btn z-depth-0">Login</button>
-            )}
-            <div className="red-text center">
-              {authError ? <p> {authError} </p> : null}
-            </div>
-          </div>
-        </form>
-        <Link to="/signup">Do not have an account? &nbsp;</Link>
-        <Link to="/forgot">Forgot password?</Link>
-      </div>
-    );
-  }
+  return (
+    <div className="container">
+      <form className="grey lighten-4" onSubmit={handleSubmit}>
+        <h5 className="grey-text text-darken-3">Sign In</h5>
+        <div className="input-field">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" onChange={handleChange} />
+        </div>
+        <div className="input-field">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" onChange={handleChange} />
+        </div>
+        <div className="input-field">
+          {isLoggedIn() ? (
+            <p>You are logged in</p>
+          ) : (
+            <button className="btn z-depth-0">Login</button>
+          )}
+        </div>
+      </form>
+      <Link to="/signup">Do not have an account? &nbsp;</Link>
+      <Link to="/forgot">Forgot password?</Link>
+    </div>
+  );
 }
 
-const mapStateToProps = state => {
-  return {
-    authError: state.auth.authError,
-    isLoggedIn: state.auth.isLoggedIn
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    signInUser: credentials => {
-      dispatch(signInUser(credentials));
-    },
-    logOutUser: token => {
-      dispatch(logOutUser(token));
-    }
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default Login;
--- webclient/src/pages/users/Signup.js
+++ webclient/src/pages/users/Signup.js
@@ -1,87 +1,57 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { signUpUser } from '../../store/actions/authActions';
+import React from 'react';
+import { useAuth } from '~/context/auth-context';
 
-class Signup extends Component {
-  state = {
+function Signup(props) {
+  const [state, setState] = React.useState({
     email: '',
     password: '',
     firstName: '',
     lastName: ''
-  };
+  });
 
-  handleReload = e => {
-    window.location.replace('/');
-  };
+  const authClient = useAuth();
 
-  handleChange = e => {
-    this.setState({
+  const handleChange = e => {
+    setState({
+      ...state,
       [e.target.id]: e.target.value
     });
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    const awaitSignUp = new Promise((resolve, reject) => {
-      this.props.signUpUser(this.state);
-      setTimeout(() => resolve('Sign Up Success'), 500);
-    });
-    awaitSignUp.then(value => {
-      this.handleReload();
-    });
+    authClient.register(state);
   };
 
-  render() {
-    const { auth, authError } = this.props;
-    //if (auth.uid) return <Redirect to='/' />
+  return (
+    <div className="container">
+      <form className="grey lighten-4" onSubmit={handleSubmit}>
+        <h5 className="grey-text text-darken-3">Sign Up</h5>
+        <div className="input-field">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" onChange={handleChange} />
+        </div>
 
-    return (
-      <div className="container">
-        <form className="grey lighten-4" onSubmit={this.handleSubmit}>
-          <h5 className="grey-text text-darken-3">Sign Up</h5>
-          <div className="input-field">
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" onChange={this.handleChange} />
-          </div>
+        <div className="input-field">
+          <label htmlFor="firstName">FirstName</label>
+          <input type="text" id="firstName" onChange={handleChange} />
+        </div>
 
-          <div className="input-field">
-            <label htmlFor="firstName">FirstName</label>
-            <input type="text" id="firstName" onChange={this.handleChange} />
-          </div>
+        <div className="input-field">
+          <label htmlFor="lastName">LastName</label>
+          <input type="text" id="lastName" onChange={handleChange} />
+        </div>
 
-          <div className="input-field">
-            <label htmlFor="lastName">LastName</label>
-            <input type="text" id="lastName" onChange={this.handleChange} />
-          </div>
-
-          <div className="input-field">
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" onChange={this.handleChange} />
-          </div>
-          <div className="input-field">
-            <button className="btn z-depth-0">Sign Up</button>
-            <div className="red-text center">
-              {authError ? <p>{authError}</p> : null}
-            </div>
-          </div>
-        </form>
-      </div>
-    );
-  }
+        <div className="input-field">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" onChange={handleChange} />
+        </div>
+        <div className="input-field">
+          <button className="btn z-depth-0">Sign Up</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-const mapStateToProps = state => {
-  return {
-    authError: state.auth.authError
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    signUpUser: credentials => {
-      dispatch(signUpUser(credentials));
-    }
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Signup);
+export default Signup;
