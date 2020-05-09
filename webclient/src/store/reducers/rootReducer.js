de444d6e34aca704960d6d01d237adff37ab34d1
Bennett Clement <bennettclement@gmail.com>
2020-05-09 13:05:09 +0800

    Delete unneeded files

--- webclient/src/store/reducers/rootReducer.js
+++ webclient/src/store/reducers/rootReducer.js
@@ -1,9 +1,7 @@
-import authReducer from './authReducer';
 import planReducer from './planReducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
-  auth: authReducer,
   plan: planReducer
 });
 
