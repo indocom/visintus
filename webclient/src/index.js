b9b6c0c048a82d2855c037641c31f5aaf0f3a59e
Bennett Clement <bennettclement@gmail.com>
2020-05-09 13:02:47 +0800

    Change class to functions

--- webclient/src/index.js
+++ webclient/src/index.js
@@ -7,12 +7,15 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
+import AppProviders from './context';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <AppProviders>
+      <App />
+    </AppProviders>
   </Provider>,
   document.getElementById('root')
 );
