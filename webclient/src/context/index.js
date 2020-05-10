29ca0c42ee637bcc7c7634fb12e3813b0fdafabf
Bennett Clement <bennettclement@gmail.com>
2020-05-10 14:41:41 +0800

    Add itin context

--- webclient/src/context/index.js
+++ webclient/src/context/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { AuthProvider } from './auth-context';
 import { UserProvider } from './user-context';
+import { ItinProvider } from './itin';
 
 function AppProviders({ children }) {
   return (
     <AuthProvider>
-      <UserProvider>{children}</UserProvider>
+      <UserProvider>
+        <ItinProvider>{children}</ItinProvider>
+      </UserProvider>
     </AuthProvider>
   );
 }
